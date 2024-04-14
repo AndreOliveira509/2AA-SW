6673928adfef18427717f396fcbbf38fa39ff7ef
André Oliveira <131814071+AndreOliveira509@users.noreply.github.com>
2024-04-14 12:08:53 -0300

    Add files via upload

---
+++ Aula004/aula004.php
@@ -0,0 +1,33 @@
+<?php 
+    echo "<br>";
+    echo ("pi: " . pi());
+    echo "<br>";
+    echo "###############################################################";
+    echo "<br>";
+    echo(min(0, 150, 30, 20, -8, -200));
+    echo "<br>";
+    echo "###############################################################";
+    echo "<br>";
+    echo(min(0, 150, 30, 20, -8, -200));
+    echo "<br>";
+
+    $num = rand(0,100);
+    echo("Raiz quadrada de ". $num . " = " . sqrt($num));
+    
+    echo "<br>";
+    echo "###############################################################";
+    echo "<br>";
+    echo("Numeros Aleatorios: " . rand());
+    echo "<br>";
+    echo "###############################################################";
+    echo "<br>";
+    echo("Letra Aleatoria: " . $letra = chr(64+rand(0,25))); 
+    echo($letra = chr(64+rand(0,25))); 
+    echo($letra = chr(64+rand(0,25)));
+    echo($letra = chr(64+rand(0,25))); 
+    echo "<br>";
+    echo "###############################################################";
+    //fazer raiz quadrada com numero randomico
+    
+
+?>
