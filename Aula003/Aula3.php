69c5ac629d8df1c809cae6541f71446643dc7f48
André Oliveira <131814071+AndreOliveira509@users.noreply.github.com>
2024-04-14 12:08:43 -0300

    .
    .

---
+++ Aula003/Aula3.php
@@ -0,0 +1,19 @@
+    <?php
+    echo "Hello" . "<br>";
+    echo 'Hello' . '<br>';
+    echo 'André' . '<br>';
+    $nome = "André";
+    echo "Hello $nome" . "<br>";
+    echo 'Hello $nome' . "<br>";
+
+    echo strlen("Andre") . "<br>";
+
+    echo str_word_count("Hello") . "<br>";
+    
+    echo str_word_count("One Piece é uma série de mangá escrita e ilustrada por Eiichiro Oda. Os capítulos têm sido serializados na revista Weekly Shōnen Jump desde julho de 1997, com os capítulos compilados e publicados em 106 volumes tankōbon pela editora Shueisha até junho de 2023"). "<br>";
+    
+    echo strpos("Hello ou world", "world"). "<br>"; 
+
+    echo strlen('One Piece é uma série de mangá escrita e ilustrada por Eiichiro Oda. Os capítulos têm sido serializados na revista Weekly Shōnen Jump desde julho de 1997, com os capítulos compilados e publicados em 106 volumes tankōbon pela editora Shueisha até junho de 2023');
+
+    ?>
